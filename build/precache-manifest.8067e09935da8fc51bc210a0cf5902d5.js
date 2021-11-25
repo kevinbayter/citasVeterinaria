0f2ed8dcdd2687bc24f93889d5eb27d107435b16
Kevin Bayter <64910815+kevinbayter@users.noreply.github.com>
2021-11-24 22:39:50 -0500

    build upload

---
+++ build/precache-manifest.8067e09935da8fc51bc210a0cf5902d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bf008018546503fd01a5f71ac995ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788cda09d4d40ce250ab",
+    "url": "/static/css/main.9f3a61e1.chunk.css"
+  },
+  {
+    "revision": "4eeb61d24d793d114467",
+    "url": "/static/js/2.e0ca3766.chunk.js"
+  },
+  {
+    "revision": "788cda09d4d40ce250ab",
+    "url": "/static/js/main.72e1f6c1.chunk.js"
+  },
+  {
+    "revision": "4acd4b0b2e3598d28910",
+    "url": "/static/js/runtime-main.4a5b470e.js"
+  }
+]);
